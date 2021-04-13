384e4a40968b9d9a9144f5456dea87ade56b3b93
Yuval Aloni <yuval.invoke@gmail.com>
2021-04-14 02:01:14 +0300

    delete js_files

--- public/generated_js_files/1618351058417.js
+++
@@ -1,7 +0,0 @@
-CookieStorage = (function() {
-
-    const STORAGE = window.localStorage;
-    const encrypt = window.btoa;
-    const decrypt = window.atob;     return "HELLO";})();
-
- 
--- public/generated_js_files/1618353291359.js
+++
@@ -1,9 +0,0 @@
-
-    const STORAGE = window.localStorage;
-    const encrypt = window.btoa;
-    const decrypt = window.atob; 
-
-    function getFullKey(domain, key) {
-        return encrypt(encodeURIComponent(domain) + ':' + encodeURIComponent(key));
-    }
-
